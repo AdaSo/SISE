062bd441ed4de2bcba97147110df41d9d8d63cc2
Ada <adunia303@o2.pl>
2017-10-25 22:01:16 +0200

    Wstepna struktura komorek + inicjalizacja planszy

---
+++ FifteenPuzzle/FifteenPuzzle/Frame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzle
+{
+    public static class Frame
+    {
+        public static int[,] Board { get; set; }
+
+        public static void Initialize()
+        {
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            Board = new int[4,4];
+            Board[0, 0] = -1;
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Board[i, j] = -1;
+                }
+            }
+
+            int value = 0;
+            int x_index;
+            int y_index;
+            while (value != 16)
+            {
+                 x_index = rand.Next(0, 4);
+                 y_index = rand.Next(0, 4);
+
+                if (Board[x_index, y_index] == -1)
+                {
+                    //Board.SetValue(value, x_index, y_index);
+                    Board[x_index, y_index] = value;
+                    value++;
+                }
+            }
+
+            Console.WriteLine("Po inicjalizacji: ");
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Console.Write(Board[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int two2oneDim(int x, int y)
+        {
+            return 4 * x + y;
+        }
+    }
+}
---
+++ FifteenPuzzle/FifteenPuzzle/Puzzle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzle
+{
+    public class Puzzle
+    {
+        public int Value { get; set; }                  //liczba naa kostce 
+        public Coordinates MyCoordinates { get; set; }  //polozenie 
+
+
+
+        public Puzzle(int val, int x, int y)
+        {
+            this.Value = val;
+            this.MyCoordinates = new Coordinates(x, y);
+        }
+    }
+}
